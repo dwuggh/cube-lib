6487553d7d4458116db2907c0e52e85f45783b2d
dwuggh <dwuggh@gmail.com>
2020-07-25 12:09:30 +0800

    update

--- rollup.config.js
+++ rollup.config.js
@@ -5,46 +5,78 @@ import external from 'rollup-plugin-peer-deps-external'
 import resolve from '@rollup/plugin-node-resolve'
 // import resolve from 'rollup-plugin-node-resolve'
 import image from '@rollup/plugin-image'
+import url from '@rollup/plugin-url'
+// import pkg from './package.json'
+// import alias from '@rollup/plugin-alias'
+// import json from "@rollup/plugin-json";
 
-import pkg from './package.json'
+// const pkg = process.env.LERNA_PACKAGE_NAME &&
+//       require(`${process.env.LERNA_PACKAGE_NAME}/package.json`);
 
+// const dependencies = ({ dependencies }) => Object.keys(dependencies || {});
+
+// const pkgdependencies = dependencies(pkg);
+// function glsl() {
+// 	return {
+// 		transform( code, id ) {
+// 			if ( /\.glsl.js$/.test( id ) === false ) return;
+// 			code = code.replace( /\/\* glsl \*\/`((.|\n)*)`/, function ( match, p1 ) {
+// 				return JSON.stringify(
+// 					p1
+// 						.trim()
+// 						.replace( /\r/g, '' )
+// 						.replace( /[ \t]*\/\/.*\n/g, '' ) // remove //
+// 						.replace( /[ \t]*\/\*[\s\S]*?\*\//g, '' ) // remove /* */
+// 						.replace( /\n{2,}/g, '\n' ) // # \n+ to \n
+// 				);
+// 			} );
+// 			return {
+// 				code: code,
+// 				map: { mappings: '' }
+// 			};
+// 		}
+// 	};
+// }
 export default {
-  input: 'src/cubes/Hexahedron.ts',
+  input: 'src/index.ts',
   output: [
     {
-      file: "puzzling.iife.js",
+      file: "build/puzzling-lib.iife.js",
       format: 'iife',
       indent: '\t',
       name: 'test',
       // exports: 'named',
     },
     {
-      file: pkg.main,
+      file: "build/puzzling-lib.js",
       format: 'umd',
       name: 'PUZZLING',
       indent: '\t',
-      // exports: 'named',
-      // sourcemap: true
+      exports: 'named',
+      sourcemap: true
     },
     {
-      file: pkg.module,
+      file: "build/puzzling-lib.module.js",
       format: 'es',
       exports: 'named',
-      // sourcemap: true
+      sourcemap: true
     }
   ],
   plugins: [
+    external(),
+    resolve(),
+    image(),
+    // url(),
     typescript({
       // rollupCommonJSResolveHack: true,
       exclude: '**/__tests__/**',
       // clean: true
-      // include: ['src/**/*.ts']
+      include: ['src/**/*.ts',
+               'src/**/*.png']
     }),
-    external(),
-    resolve(),
-    image(),
     commonjs({
       include: ['node_modules/**']
     })
-  ]
+  ],
+  // external: id => pkgdependencies.includes(id)
 }
